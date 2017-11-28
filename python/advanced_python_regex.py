d97d7a84f993274c86197c52062a8458f3906b0d
hectorhcuevas <hhfire94@gmail.com>
2017-11-28 15:37:27 -0800

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,5 +1,15 @@
+Q1:
 def Faculty_degrees():
     import pandas as pd
     df2 = pd.read_csv('faculty.csv', delimiter=',')
     print(df2[' degree'].value_counts())
+Q2:
+def email_list():
+    import pandas as pd
+    df2 = pd.read_csv('faculty.csv', delimiter=',')
+    lst = []
+    for email in df2[' email']:
+        if email not in lst:
+            lst.append(email)
+            print(lst[-1])
     
