3b32e33f2c1bdad5a246e44b734972c69c9604e7
hectorhcuevas <hhfire94@gmail.com>
2017-11-30 14:49:46 -0800

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -21,21 +21,18 @@ Q6:
         
 Q7:
   
-with open('faculty.csv', mode='r') as infile:
+  with open('faculty.csv', mode='r') as infile:
     reader = csv.reader(infile)
     name = []
     for rows in reader:
         name = rows[0].partition(" ")
         #print(name)
-        last = name[-1]
-        first = name[0]
+        last = (name[-1])
+        first = (name[0])
         v = rows[1:]
         if "." in last:
             Middle_initial = last[0]
             last = last[3:]
-        print({(first,last):v})
-        '''
-        mydict = {last:v} <-- Output is right but this dictionary only outputs final name:values. 
-                                find a way to create a dictionary from output.
-        '''
-    #mydict = {last:v}
+        for x in name:
+            mydict = {first,last:v}
+        print(mydict)
