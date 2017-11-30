927b68cbc03df2008bb083f9e9129c9da3116ace
hectorhcuevas <hhfire94@gmail.com>
2017-11-29 17:16:08 -0800

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,17 @@
 PLACE YOUR CODE HERE
+email = []
+def email_list():
+    import csv
+    with open('faculty.csv', 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            from collections import Counter
+            if row[2] not in email: 
+                email.append(row[3])
+        print(email)
+        
+import csv
+
+with open("emails.csv",'w') as resultFile:
+    wr = csv.writer(resultFile)
+    wr.writerow(email)
