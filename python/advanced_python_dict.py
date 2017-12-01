a2a791757aa1904a838355a523d5bf9456340211
hectorhcuevas <hhfire94@gmail.com>
2017-12-01 14:13:41 -0800

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -6,12 +6,9 @@ reader = csv.reader(open('faculty.csv'))
 result = {}
 for row in reader:
     name = row[0]
-    full = name.partition(" ")
+    full = name.split()
     last = full[-1]
     first = full[0]
-    if "." in last:
-        middle = last[0]
-        last = name[3:]
     result[last] = row[1:]
         
         
@@ -23,10 +20,7 @@ reader = csv.reader(open('faculty.csv'))
 result = {}
 for row in reader:
     name = row[0]
-    full = name.partition(" ")
+    full = name.split()
     last = full[-1]
     first = full[0]
-    if "." in last:
-        middle = last[0]
-        last = name[3:]
     result[last,first] = row[1:]
